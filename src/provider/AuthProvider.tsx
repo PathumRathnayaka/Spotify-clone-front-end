6d0358223d15ef2f45285a957edc6ee64ced8460
Pathum Rathnayaka <thilinapathumrathnayaka@gmail.com>
2025-03-12 15:50:21 +0530

    Add authProvider

---
+++ src/provider/AuthProvider.tsx
@@ -0,0 +1,38 @@
+import { axiosInstance } from "@/lib/axios";
+import { useAuth } from "@clerk/clerk-react"
+import { Loader } from "lucide-react";
+import React, { useEffect, useState } from "react"
+
+const updateApitoken = (token: string | null) => {
+    if (token) axiosInstance.defaults.headers.common["Authorization"] = `Bearer $(token)`;
+    else delete axiosInstance.defaults.headers.common["Authorization"];
+}
+
+const AuthProvider = ({children}:{ children:React.ReactNode}) =>{
+    const {getToken } = useAuth()
+    const [loading, setLoading] = useState(true)
+
+    useEffect(() => {
+        const initAuth =  async () =>{
+            try {
+                const token = await getToken();
+                updateApitoken(token);
+            } catch (error:any) {
+                updateApitoken(null);
+                console.log("Error in auth provider",error)
+            } finally{
+                setLoading(false);
+            }
+        };
+        initAuth();
+    },[getToken]);
+
+    if(loading) return (
+    <div className="h-screen w-full flex items-center justify-center">
+        <Loader className="animate-spin h-10 w-10 text-blue-500" />
+    </div>)
+
+    return <>{children}</>
+};
+
+export default AuthProvider

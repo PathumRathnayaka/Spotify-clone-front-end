28d2d3f36d6ff42a074f700b9109932763ed0530
Pathum Rathnayaka <thilinapathumrathnayaka@gmail.com>
2025-03-30 16:52:16 +0530

    SectionGrid create

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -2,6 +2,8 @@ import TopBar from '@/components/TopBar'
 import React, { useEffect } from 'react'
 import { useMusicStore } from "@/stores/useMusicStore";
 import FeaturedSection from './component/FeaturedSection';
+import { ScrollArea } from '@/components/ui/scroll-area';
+import SectionGrid from './component/SectionGrid';
 
 const HomePage = () => {
   const {
@@ -23,10 +25,20 @@ const HomePage = () => {
 
 
   return (
-    <div className='rounded-md overflow-hidden'>
+    <main className='rounded-md overflow-hidden h-full bg-gradient-to-b from-zinc-800 to-zinc-900'>
       <TopBar/>
-      <FeaturedSection/>
-    </div>
+      <ScrollArea className='h-[calc(100vh-180px)]'>
+		<div className='p-4 sm:p-6'>
+			<h1 className='text-2xl sm:text-3xl font-bold mb-6'>Good afternoon</h1>
+			<FeaturedSection/>
+		
+		<div className='space-y-8'>
+			<SectionGrid title="Made For You" songs={madeForYouSongs} isLoading={isLoading}/>
+			<SectionGrid title="Trending" songs={trendingSongs} isLoading={isLoading}/>
+		</div>
+		</div>
+	  </ScrollArea>
+    </main>
   )
 }
 
--- src/pages/home/component/SectionGrid.tsx
+++ src/pages/home/component/SectionGrid.tsx
@@ -1,8 +1,39 @@
+import { Song } from '@/types';
 import React from 'react'
+import SectionGridSkeleton from './SectionGridSkeleton';
+import { Button } from '@/components/ui/button';
 
-const SectionGrid = () => {
+type SectionGridProps = {
+  title: string;
+  songs: Song[];
+  isLoading: boolean;
+}
+
+const SectionGrid = ({ songs, title, isLoading}:SectionGridProps) => {
+  if(isLoading) return <SectionGridSkeleton/>;
+  console.log(title,songs)
   return (
-    <div>SectionGrid</div>
+    <div className='mb-8'>
+      <div className='flex items-center justify-between mb-4'>
+        <h2 className='text-xl sm:text-2xl font-bold'>{title}</h2>
+        <Button variant='link' className='text-sm text-zinc-400 hover:text-white'>
+          show all
+        </Button>
+      </div>
+      <div>
+        {songs.map((songs) => (
+          <div key={songs._id} 
+          className='bg-zinc-800/40 p-4 rounded-md hover:bg-zinc-700/40 transition-all group cursor-pointer'>
+            <div className='relative mb-4'>
+              <div className='aspect-square rounded-md shadow-lg overflow-hidden'>
+                <img src={songs.imageUrl} alt={songs.title} 
+                className='w-full h-full object-cover transition-transform duration-300 group-hover:scale-105'/>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
   )
 }
 
